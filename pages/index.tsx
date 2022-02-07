b5c81eb4da188ed57eda37af0d9b90de160e9667
alux <ahmadabd13741112@gmail.com>
2022-02-07 17:46:57 +0330

    home page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,49 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+
+type FoodType = {
+    id: number,
+    name: string,
+    city: string,
+    country: string,
+}
+
+interface FoodProps {
+  foods: FoodType 
+}
+
+
+const Home: NextPage<FoodProps> = ({foods}) => {  
   return (
     <div className={styles.container}>
       <h2>All Foods</h2>
+      <div>
+        {listFoods(foods)}
+      </div>
     </div>
   )
+}
+
+const listFoods = (foods: FoodType) => {
+  // return foods.map((food: any) => {
+    return (
+      <div key={foods.id}>
+        <h3 className={styles.foods}>{foods.name}</h3>
+      </div>
+    )
+  // });
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch('http://localhost:5000/api/food/random')
+  const data = await response.json()
+ 
+  return {
+    props: {
+      foods: data
+    }
+  }
 }
 
 export default Home
