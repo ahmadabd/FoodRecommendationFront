840422f228927322931e3f22bda61d91954d99be
alux <ahmadabd13741112@gmail.com>
2022-02-11 11:41:00 +0330

    get all foods completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,11 @@ type FoodType = {
     name: string,
     city: string,
     country: string,
+    vegetarian: string
 }
 
 interface FoodProps {
-  foods: FoodType 
+  foods: FoodType[] | []
 }
 
 
@@ -18,25 +19,36 @@ const Home: NextPage<FoodProps> = ({foods}) => {
   return (
     <div className={styles.container}>
       <h2>All Foods</h2>
-      <div>
-        {listFoods(foods)}
+      <div className={styles.foods}>
+        <table className={styles.table}>
+          <tr className={styles.tr}>
+            <th className={styles.th}>Name</th>
+            <th className={styles.th}>City</th>
+            <th className={styles.th}>Country</th>
+            <th className={styles.th}>Vegeterian</th>
+          </tr>
+          {listFoods(foods)}
+        </table>
       </div>
     </div>
   )
 }
 
-const listFoods = (foods: FoodType) => {
-  // return foods.map((food: any) => {
+const listFoods = (foods: FoodType[]) => {
+  return foods.map((food: FoodType) => {
     return (
-      <div key={foods.id}>
-        <h3 className={styles.foods}>{foods.name}</h3>
-      </div>
+      <tr key={food.id}>
+        <td className={styles.td}>{food.name}</td>
+        <td className={styles.td}>{food.city}</td>
+        <td className={styles.td}>{food.country}</td>
+        <td className={styles.td}>{food.vegetarian}</td>
+      </tr>
     )
-  // });
+  });
 }
 
 export const getStaticProps = async () => {
-  const response = await fetch('http://localhost:5000/api/food/random')
+  const response = await fetch('http://localhost:5000/api/food/all')
   const data = await response.json()
  
   return {
